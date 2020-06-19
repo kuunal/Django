2048e2a5e170faab5998ac6eb9a674382c596038
250399 <kunaldeshmukh2503@gmail.com>
2020-06-18 18:44:39 -0700

    [Kunal] ADD: Same email cant register again

--- register/views.py
+++ register/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse, JsonResponse
 from .forms import RegisterForms
 from .models import Register as r
+from django.contrib import messages
 
 # Create your views here.
 def register_user(request):
@@ -10,8 +11,13 @@ def register_user(request):
         last_name=request.POST['lastname']        
         email=request.POST['email']        
         password=request.POST['password']
-        r.objects.create(first_name=first_name,last_name=last_name,email=email,password=password)
-        return redirect('login')
+        if(r.objects.filter(email=email)):
+            print("Insidddeeeeeeeeeeeeeeeeee")
+            messages.warning(request, f'Email already Registered!')
+            return render(request ,'register/register.html')
+        else:
+            r.objects.create(first_name=first_name,last_name=last_name,email=email,password=password)
+            return redirect('login')
     else:
         return render(request ,'register/register.html')
         
