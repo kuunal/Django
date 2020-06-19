db36ca7c12cda242213473449bc5c9721bed95bc
250399 <kunaldeshmukh2503@gmail.com>
2020-06-18 23:19:44 -0700

    [Kunal] ADD: basic code for login

--- login/views.py
+++ login/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Login
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def validate(request):
     if request.method == 'POST':
-        form = AuthenticationForm( data = request.POST )
-        if form.is_valid():
+        user = authenticate(email=request.POST['email'], password=request.POST['password'])
+        print(user,"SAdSadASDSADASs")
+        if user:
             return JsonResponse({'success':True})
         else:
             return JsonResponse({'success':False})
